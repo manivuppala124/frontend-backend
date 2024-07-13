555d774819d4e2e5c881bc9db7c92ba0be2e7bba
manivuppala124 <154235442+manivuppala124@users.noreply.github.com>
2024-07-13 20:17:20 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+
+export { default as MapView } from './MapView';
+export { default as HotelsList } from './HotelsList';
+export { default as RestaurantsList } from './RestaurantsList';
+export { default as AttractionsList } from './AttractionsList';
+export { default as SearchResult } from './SearchResult';
+export { default as NotFound } from './NotFound';
+export { default as Home } from './Home';
